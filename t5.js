36d264a73a1263c77cbc81a4a36c4790d6d4ae1a
sephirothz87 <sephiroth.zhong@gmail.com>
2017-11-29 09:54:33 +0800

    P4 https://leetcode.com/problemset/all/ 使用新的算法通过

--- t5.js
+++ t5.js
@@ -1,39 +1,51 @@
 //https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
 var longestPalindrome = function(s) {
-    var s_array = s.split('');
-    var map = {};
-    var cur_len = 0;
+    var start = 0;
+    var end = 0;
     var max_len = 0;
-    var start_cursor = 0;
+    var s_a = s.split('');
+    // var ret = s_a[0];
 
-    for(let i=0;i<s_array.length;i++){
-        if(map[s_array[i]] !== undefined){
-            // cur_len++;
-            // map[s_array[i]] = i;
-            cur_len = i-map[s_array[i]]+1;
-            if(cur_len>max_len){
-                max_len = cur_len;
-                i = map[s_array[i]];
-                start_cursor = i;
-                map = {};
+    for(let i=0;i<s_a.length;i++){
+        var len1 = fromMid(s_a,i,i);
+        var len2 = fromMid(s_a,i,i+1);
+
+        if(len1==1&&len2==0){
+            continue;
+        }
+
+        var len = Math.max(len1,len2);
+
+        if(len>max_len){
+            max_len = len;
+            if(len%2==0){
+                start = i-(len-2)/2;
+                end = i+len/2;
+            }else{
+                start = i-(len-1)/2;
+                end = i+(len-1)/2;
             }
-        }else{
-            map[s_array[i]] = i;
         }
     }
-
-    console.log(max_len);
-    if(max_len>0){
-        return s_array.slice(start_cursor,start_cursor+max_len).join('');
+    if(max_len!=0){
+        return s_a.slice(start,end+1).join('');
     }else{
-        return s_array[0];
+        return s_a[0];
     }
 };
 
+var fromMid = (s_a,left,right)=>{
+    while(s_a[left]!==undefined && s_a[right]!==undefined && s_a[left]==s_a[right]){
+        left--;
+        right++;
+    }
+    return right-left-1;
+};
 
-// s = "babad";
-// s = "cbbd";
+
+s = "babad";
+s = "cbbd";
 s = "a";
 
 // s_a = s.split('');
