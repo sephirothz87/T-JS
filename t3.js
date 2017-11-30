9ca6c17c5f2e909a2f9b9fbfc2bf7fdd75f84a41
SephirothZ87 <sephiroth.zhong@gmail.com>
2017-11-30 17:17:34 +0800

    https://leetcode.com/problems/longest-substring-without-repeating-characters/description/ 更新为查询map算法

--- t3.js
+++ t3.js
@@ -1,36 +1,63 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-//最终版
+
+
+
+
+//效率极差，重做
+//思路，用一个对象存每一个值最后一次出现的位置，然后计算出遍历整个字符串后的最大值
 var lengthOfLongestSubstring = function(s) {
-    var array = s.split("");
-    if(array.length==1){
-        return 1;
+    let map={},cur=0,len=0;
+
+    for(let i=0;i<s.length;i++){
+        if(map[s[i]]){cur = Math.max(map[s[i]],cur);}
+        len = Math.max(len,i-cur+1);
+        map[s[i]] = i+1;
     }
 
-    var max_length = 0;
-    var cur_length = 0;
-
-    var map = {};
-
-    for (i=0;i<array.length;i++) {
-        if(map[array[i]] == undefined) {
-            map[array[i]] = i;
-            cur_length++;
-        }else{
-            if(cur_length>max_length){
-                max_length = cur_length;
-            }
-            cur_length = 0;
-            i = map[array[i]];
-            map = {}
-        }
-    }
-
-    if(cur_length>max_length){
-        return cur_length;
-    }
-
-    return max_length;
+    return len;
 };
+
+// var a = "abcabcbb";
+// var a = "bbbbb";
+// var a = "pwwkew";
+// var a = "c";
+var a = "";
+
+console.log(lengthOfLongestSubstring(a));
+//效率极差，重做
+
+//最终版
+// var lengthOfLongestSubstring = function(s) {
+//     var array = s.split("");
+//     if(array.length==1){
+//         return 1;
+//     }
+//
+//     var max_length = 0;
+//     var cur_length = 0;
+//
+//     var map = {};
+//
+//     for (i=0;i<array.length;i++) {
+//         if(map[array[i]] == undefined) {
+//             map[array[i]] = i;
+//             cur_length++;
+//         }else{
+//             if(cur_length>max_length){
+//                 max_length = cur_length;
+//             }
+//             cur_length = 0;
+//             i = map[array[i]];
+//             map = {}
+//         }
+//     }
+//
+//     if(cur_length>max_length){
+//         return cur_length;
+//     }
+//
+//     return max_length;
+// };
 //最终版
 
 
@@ -57,15 +84,3 @@ var lengthOfLongestSubstring = function(s) {
 //     return max_length;
 // };
 
-
-// var a = "abcabcbb";
-// var a = "bbbbb";
-var a = "pwwkew";
-
-a_a = a.split("");
-
-console.log(a_a);
-
-
-
-console.log(lengthOfLongestSubstring(a));
