453553dd74ff73c7af5de9c381ee23bfe625d59b
sephirothz87 <sephiroth.zhong@gmail.com>
2018-01-14 13:26:27 +0800

    no message

--- t.js
+++ t.js
@@ -13,49 +13,60 @@ var obj = {
     '4l' : function(x){console.log(' l ' + x)},
 }
 
-
-// console.log(obj.a)//a
-// console.log(obj.b)//b
-// console.log(obj.c)//function(){xxxxx}
-// console.log(obj.d)//function(){xxxxx}
-// console.log(obj.e)//function(){xxxxx}
-// console.log(obj.f)//function(){xxxxx}
-// console.log(obj.g)//(x)=>{xxxx}
-// console.log(obj.h)//(x)=>{xxxx}
+///////////////////////////////////////////////////////////////
+console.log(obj.a)//a
+console.log(obj.b)//b
+console.log(obj.c)//function(){xxxxx}
+console.log(obj.d)//function(){xxxxx}
+console.log(obj.e)//function(){xxxxx}
+console.log(obj.f)//function(){xxxxx}
+console.log(obj.g)//(x)=>{xxxx}
+console.log(obj.h)//(x)=>{xxxx}
 
 // console.log(obj.2j)//不可以这样写,报错
 // console.log(obj.4l)//不可以这样写,报错
+///////////////////////////////////////////////////////////////
 
 
 
 
+///////////////////////////////////////////////////////////////
 // console.log(obj[a])//不可以这样写,报错
+///////////////////////////////////////////////////////////////
 
 
 
 
-// console.log(obj['a'])//a
-// console.log(obj['b'])//b
-// console.log(obj['c'])//function(){xxxxx}
-// console.log(obj['d'])//function(){xxxxx}
-// console.log(obj['e'])//function(){xxxxx}
-// console.log(obj['f'])//function(){xxxxx}
-// console.log(obj['g'])//(x)=>{xxxx}
-// console.log(obj['h'])//(x)=>{xxxx}
-// console.log(obj['2j'])//j
-// console.log(obj['4l'])//function(){xxxxx}
+///////////////////////////////////////////////////////////////
+console.log(obj['a'])//a
+console.log(obj['b'])//b
+console.log(obj['c'])//function(){xxxxx}
+console.log(obj['d'])//function(){xxxxx}
+console.log(obj['e'])//function(){xxxxx}
+console.log(obj['f'])//function(){xxxxx}
+console.log(obj['g'])//(x)=>{xxxx}
+console.log(obj['h'])//(x)=>{xxxx}
+console.log(obj['2j'])//j
+console.log(obj['4l'])//function(){xxxxx}
+///////////////////////////////////////////////////////////////
 
 
 
-// obj.c();//c
-// obj.d();//d
-// obj.e('x');//e x
-// obj.f('x');//f x
-// obj.g('x');//g x
-// obj.h('x');//h x
+///////////////////////////////////////////////////////////////
+obj.c();//c
+obj.d();//d
+obj.e('x');//e x
+obj.f('x');//f x
+obj.g('x');//g x
+obj.h('x');//h x
 
 // obj.4l(x);//不可以这样写,报错
+///////////////////////////////////////////////////////////////
 
+
+
+
+///////////////////////////////////////////////////////////////
 obj['c']();//c
 obj['d']();//d
 obj['e']('x');//e x
@@ -63,3 +74,4 @@ obj['f']('x');//f x
 obj['g']('x');//g x
 obj['h']('x');//h x
 obj['4l']('x');//4l x
+///////////////////////////////////////////////////////////////
