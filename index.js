a4ecb7160f88a605c2d428b12cce616c045f8e0a
sephirothz87 <sephiroth.zhong@gmail.com>
2018-01-21 18:32:54 +0800

    5.5  Function

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var log4 = (x)=>{
+function log4(x){
     console.log(x);
     console.log('valueOf()',x.valueOf());
     console.log('toString()',x.toString());
     console.log('toLocalString()',x.toLocaleString());
-};
+}
+
+function logAll(x){
+    console.log(x);
+    console.log(typeof x);
+    console.log('valueOf()',x.valueOf());
+    console.log('toString()',x.toString());
+    console.log('toLocalString()',x.toLocaleString());
+}
