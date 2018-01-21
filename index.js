c54ecf7afea5f66061c955d8ea0ca066d7fa7178
sephirothz87 <sephiroth.zhong@gmail.com>
2018-01-21 15:34:28 +0800

    5.4  RegExp

---
+++ index.js
@@ -0,0 +1,6 @@
+var log4 = (x)=>{
+    console.log(x);
+    console.log('valueOf()',x.valueOf());
+    console.log('toString()',x.toString());
+    console.log('toLocalString()',x.toLocaleString());
+};
