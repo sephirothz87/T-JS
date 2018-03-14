2d2599f532bffa52f85312595f58e8ac7d0d997c
sephirothz87 <sephiroth.zhong@gmail.com>
2018-03-14 16:53:29 +0800

    no message

--- t4.js
+++ t4.js
@@ -1,104 +1,57 @@
 //在字符串中找到最长的合法括号组合
-// var longestValidParentheses = function(s) {
-// 	let max = 0;
-// 	let stackL = [];
-//     let len = 0;
-//     let tmpLen = 0;
-//     let lastLen = 0;
-//     let lastPop = 0;
 
-// 	for(let i=0;i<s.length;i++){
-// 		if(s.charAt(i) == '('){
-// 			stackL.push(i);
-// 		} else if(s.charAt(i) == ')') {
-// 			if(stackL.length == 0 && len>0){
-// 				if(len>max){
-// 					max = len;
-// 					len = 0;
-// 				}
-// 				continue;
-// 			}
-
-// 			if(stackL.length>0){
-// 				tmpLen = i - stackL.pop() +1
-// 				max = Math.max(tmpLen,max);
-// 				if(stackL.length == 0){
-// 					len+=tmpLen;
-// 					// max = Math.max(len,max);
-// 				}
-// 			}
-
-// 		} else {
-// 			return 'error char'
-// 		}
-
-// 		if(s.length-1 == i){
-// 			if(stackL.length>0){
-// 				lastLen = i - stackL[stackL.length-1];
-// 			}
-// 			max = Math.max(len,tmpLen,lastLen,max);
-// 		}
-// 	}
-
-// 	return max;
-// };
-
-
+route = [];
+cursor = -1;
 
 var longestValidParentheses = function(s) {
-	let max = 0;
-	let stackL = [];
-    let cursorL = Infinity;
-    let cursorR = 0;
-    let wholeLeftCursor = Infinity;
 
 	for(let i=0;i<s.length;i++){
 		if(s.charAt(i) == '('){
-			stackL.push(i);
-		} else if(s.charAt(i) == ')') {
-			if(stackL.length == 0){
-				// max = Math.max(max,cursorR - wholeLeftCursor + 1);
-				cursorL = Infinity;
-				wholeLeftCursor = Infinity;
+			cursor++;
+
+			if(!route[cursor]){
+				route.push(0);
 			}
-
-			if(stackL.length>0){
-				lastPop = stackL.pop()
-				cursorL = Math.min(lastPop,cursorL);
-				cursorR = i;
-
-				// max = Math.max(max,cursorR - cursorL + 1);
-
-				if(stackL.length==0){
-					wholeLeftCursor = Math.min(cursorL,wholeLeftCursor);
-					max = Math.max(max,cursorR - wholeLeftCursor + 1);
-					cursorL = Infinity;
-				}
+		} else if(s.charAt(i) == ')'){
+			if(cursor>0){
+				cursor--;
+				// if(route[cursor]){
+					route[cursor]++;
+				// }else{
+				// 	route.push(1);
+				// }
 			}
-
 		} else {
 			return 'error char'
 		}
+	}
 
-		if(s.length-1 == i){
-			if(stackL.length>0){
-				max = Math.max(max,cursorR - cursorL + 1);
-			}
+	console.log(route);
+	route.sort((a,b)=>{return b-a});
+	console.log(route);
+
+	let max = 0,len = 0;
+	for(let i=0;i<route.length;i++){
+		if(route[i]>0){
+			len+=route[i]
+		}else{
+			max = Math.max(max,len);
+			len = 0;
 		}
 	}
 
-	return max;
+	return max*2;
 };
 
-
-// s1 = '(()';
-// s1 = '())';
-// s1 = ')()())';
-// s1 = "()(()";
-// s1 = "()()"
-// s1 = "(()()";
-s1 = '()';
-// s1 = ")(((((()())()()))()(()))(";
-// s1 = "(()()(())((";
+s1 = '(()';
+s1 = '())';
+s1 = ')()())';
+s1 = "()(()";
+s1 = "()()"
+s1 = "(()()";
+// s1 = '()';
+s1 = ")(((((()())()()))()(()))(";
+s1 = "(()()(())((";
+s1 = "(()(((()";
 
 console.log(longestValidParentheses(s1));
