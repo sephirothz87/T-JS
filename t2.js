4af89997095887e5b610581bef5961746aaa7315
sephirothz87 <sephiroth.zhong@gmail.com>
2018-03-14 12:40:19 +0800

    no message

--- t2.js
+++ t2.js
@@ -1,20 +1,32 @@
 //移除已排序数组中的重复值
 
+// function removeDu(nums){
+//     let number = [];
+
+//     for (let i=0;i<nums.length;i++){
+//         let v = nums[i];
+//         while(nums[i+1] === v){
+//             i++;
+//         }
+//         number.push(nums[i]);
+//     }
+
+//     return number;
+// }
+
+//不借助新的数组,不需要返回值
 function removeDu(nums){
-    let number = [];
-
-    for (let i=0;i<nums.length;i++){
-        let v = nums[i];
-        while(nums[i+1] === v){
-            i++;
+    let len = nums.length;
+    for (let i=0;i<len;i++){
+        while(nums[i+1] === nums[i]){
+            nums.splice(i+1,1);
+            len--;
         }
-        number.push(nums[i]);
     }
-
-    return number;
 }
 
 a1 = [1,1,2];
 // a1 = [1,1,2,3,4,4,4,5,6,7,7,8,8,9,10];
 
-console.log(removeDu(a1));
+removeDu(a1);
+console.log(a1);
